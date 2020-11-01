0f15cac334e413f048200febed1fc0bd4f78ab45
Huijuan Yang <huijuan.yang@Huijuans-MacBook-Pro.local>
2020-11-01 15:03:45 +0800

    finished userinfo display part

--- src/UserInfo/UserInfo.js
+++ src/UserInfo/UserInfo.js
@@ -3,7 +3,30 @@ import './UserInfo.scss';
 
 class UserInfo extends Component {
   render() {
-    return <aside className="UserInfo">User Info</aside>;
+    const { name, email, phone, website, company } = this.props.user;
+
+    return (
+      <aside className="UserInfo">
+        <h3>{name}</h3>
+        <p className="contact-display">Contact Me @</p>
+        <div className="row-display">
+          <h4>Email</h4>
+          <p className="userinfo-display">{email}</p>
+        </div>
+        <div className="row-display">
+          <h4>Phone</h4>
+          <p className="userinfo-display">{phone}</p>
+        </div>
+        <div className="row-display">
+          <h4>Website</h4>
+          <p className="userinfo-display">{website}</p>
+        </div>
+        <div className="row-display">
+          <h4>Company</h4>
+          <p>{company ? company.name : ''}</p>
+        </div>
+      </aside>
+    );
   }
 }
 
