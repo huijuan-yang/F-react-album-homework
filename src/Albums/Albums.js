3240b4eb8965b2f14abdf925a71481ecbdc433fa
Huijuan Yang <huijuan.yang@Huijuans-MacBook-Pro.local>
2020-11-01 16:54:15 +0800

    website can show each album's title

--- src/Albums/Albums.js
+++ src/Albums/Albums.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Albums.scss';
+import { fetchAlbums } from '../apiUtil';
 
 class Albums extends Component {
   constructor(props) {
@@ -9,13 +10,21 @@ class Albums extends Component {
     };
   }
 
+  componentDidMount() {
+    fetchAlbums().then((albums) => {
+      this.setState({
+        albums,
+      });
+    });
+  }
+
   render() {
     const { albums } = this.state;
     return (
       <section className="Albums">
-        {albums.map(() => (
+        {albums.map((album) => (
           <div className="album">
-            <p className="title">Album title here...</p>
+            <p className="title">{album.title}</p>
           </div>
         ))}
       </section>
