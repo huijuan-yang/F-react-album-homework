512c0fc6f138ef54349be4c58e6edb7ea696fd28
Huijuan Yang <huijuan.yang@Huijuans-MacBook-Pro.local>
2020-11-01 21:38:10 +0800

    added display prop of each album

--- src/Albums/Album.js
+++ src/Albums/Album.js
@@ -7,32 +7,48 @@ class Album extends Component {
     super(props);
     this.state = {
       photos: [],
+      display: false,
     };
   }
 
   componentDidMount() {
     const { id } = this.props;
+    const { display } = this.state;
     fetchPhotos(id).then((photos) => {
       this.setState({
         photos,
+        display,
       });
     });
   }
 
+  handleClick = () => {
+    const { display } = this.state;
+    this.setState({
+      display: !display,
+    });
+  };
+
   render() {
     const { album } = this.props;
-    const { photos } = this.state;
+    const { photos, display } = this.state;
     return (
       <div className="album">
-        <p className="title">{album.title}</p>
-        <div className="album-container">
-          {photos.slice(0, 3).map((photo) => (
-            <div className="thumbnail-container">
-              <img src={photo.thumbnailUrl} />
-              <p>{photo.title}</p>
-            </div>
-          ))}
-        </div>
+        <p className="title" onClick={this.handleClick}>
+          {album.title}
+        </p>
+        {display ? (
+          <div className="album-container">
+            {photos.slice(0, 3).map((photo) => (
+              <div className="thumbnail-container" key={photo.id}>
+                <img src={photo.thumbnailUrl} />
+                <p>{photo.title}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          ''
+        )}
       </div>
     );
   }
